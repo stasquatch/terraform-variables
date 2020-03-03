c245198968d3fecaecb7f2b4904710411d5abb8d
Peter Strzyzewski <peters2222@gmail.com>
2020-03-03 14:16:36 -0800

    Added random shuffler

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ resource "random_pet" "pet" {
 output "tell_me_my_pets_name" {
   value = "c'mon ${var.prefix} ${random_pet.pet.id}, let's go"
 }
+
+resource "random_uuid" "seed" {}
+
+resource "random_shuffle" "pet" {
+  input = split(var.chosen_separator, random_pet.pet.id)
+  seed = random_uuid.seed.result
+}
+
+output "tell_me_my_real_pets_name" {
+  value ="c'mon ${var.prefix} ${join(var.chosen_separator, random_shuffle.pet.result)}, let's go"
+}
