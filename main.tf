892504122543df5b6ab9825ef161db43df8f6c2b
Peter Strzyzewski <peters2222@gmail.com>
2020-03-04 14:28:37 -0800

    Local functions that make more sense

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+locals {
+  short_name = title(element(reverse(split(var.chosen_separator, random_pet.pet.id)), 0))
+}
 resource "random_pet" "pet" {
   length = var.pet_name_length
   separator = var.chosen_separator
@@ -7,13 +10,6 @@ output "tell_me_my_pets_name" {
   value = "c'mon ${var.prefix} ${random_pet.pet.id}, let's go"
 }
 
-resource "random_uuid" "seed" {}
-
-resource "random_shuffle" "pet" {
-  input = split(var.chosen_separator, random_pet.pet.id)
-  seed = random_uuid.seed.result
-}
-
-output "tell_me_my_real_pets_name" {
-  value ="c'mon ${var.prefix} ${join(var.chosen_separator, random_shuffle.pet.result)}, let's go"
+output "what_my_friends_say" {
+  value ="Hey ${local.short_name}, do the renegade!"
 }
